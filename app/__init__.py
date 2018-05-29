206a3780b49daa169dbf5c00e5ac690317b5b263
Mateo Marulanda <mmarulandc@eafit.edu.co>
2018-05-29 17:15:19 -0500

    arreglando

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,22 @@
 from flask import Flask, request, redirect, render_template, url_for
 
 app = Flask(__name__)
-@app.route('/')
+#@app.route('/')
+"""
 def index():
     return render_template('index.html')
+"""
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+
+
+@app.route('/lights/')
+def luces():
+    return render_template('lights.html')
+
+@app.route('/temperature/')
+def temperature():
+    return render_template('temperature.html')
+
